0129638935e7cbd73c63a24998d3c689aec491df
Sebastian <anneliina.hoiva@gmail.com>
2017-01-13 14:40:28 +0200

    Improved test file

--- test.js
+++ test.js
@@ -3,17 +3,19 @@ var sortStyle = require("./utils").sortStyle;
 
 var OPTIONS_LANG = { lang: "fi" };
 var OPTIONS_LANGS = { lang: ["fi", "sv"] };
-var OPTIONS_ADDON = { extensions: ["icons"] };
-var OPTIONS_LANGS_ADDONS = { lang: ["fi", "sv"], extensions: ["icons", "stops"]};
+var OPTIONS_ADDON_ICONS = { extensions: ["icons"] };
+var OPTIONS_ADDON_STOPS = { extensions: ["stops"] };
+var OPTIONS_ALL = { lang: ["fi", "sv"], extensions: ["icons", "stops"]};
 
-function generateStyle() {
-	var style = require("./index.js").generateStyle(OPTIONS_LANGS_ADDONS);
+function generateStyle(options, comparisonFile) {
+	var style = require("./index.js").generateStyle(options);
 	
 	fs.writeFile("test-hsl-gl-map-v9-final.json", JSON.stringify(style), function (err) {
     	if (err) return console.log(err);
-    	sortStyle("hsl-gl-map-with-stops-v9.json", "test-sorted-hsl-gl-map-with-stops-v9.json");
+    	sortStyle(comparisonFile, "test-sorted-" + comparisonFile);
     	sortStyle("test-hsl-gl-map-v9-final.json", "test-sorted-hsl-gl-map-v9-final.json");
 	});
 }
 
-generateStyle();
+//generateStyle(OPTIONS_ADDON_ICONS, "hsl-gl-map-v9.json");
+generateStyle(OPTIONS_ADDON_STOPS, "hsl-gl-map-with-stops-v9.json");
