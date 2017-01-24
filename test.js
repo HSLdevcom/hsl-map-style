4011ec38734b2ab1c61efe91463869b86918aed9
Sebastian <sebastian.hognabba@gmail.com>
2017-01-24 16:26:10 +0200

    Allow sources url and glyphs url default values to be replaced by caller

--- test.js
+++ test.js
@@ -2,6 +2,13 @@ var fs = require("fs");
 var sortStyle = require("./utils").sortStyle;
 
 var OPTIONS_LANG = { lang: "fi" };
+var OPTIONS_HSL = {
+	sourcesUrl: "hsl-map-server:8080/",
+	glyphsUrl: "file://" + process.env.WORK + "/node_modules/hsl-map-style/",
+	lang: "fi",
+	extensions: ["icons"],
+};
+
 var OPTIONS_LANGS = { lang: ["fi", "sv"] };
 var OPTIONS_ICONS = { extensions: ["icons"] };
 var OPTIONS_STOPS = { extensions: ["stops"] };
@@ -20,4 +27,4 @@ function generateStyle(options, comparisonFile) {
 	fs.writeFileSync("test-" + comparisonFile, fs.readFileSync(comparisonFile, "utf8"));
 }
 
-generateStyle(OPTIONS_ALL, "hsl-gl-map-with-stops-v9.json");
+generateStyle(OPTIONS_HSL, "hsl-gl-map-v9.json");
