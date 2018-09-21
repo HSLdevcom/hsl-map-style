59786adc02c7f3115b985b856311c0aa707c3f6c
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-09-21 12:33:04 +0300

    Sync our style with the master, create reordering command to sync layer order.

--- cli/index.js
+++ cli/index.js
@@ -1,6 +1,7 @@
 const program = require("commander");
 const split = require("./split");
 const render = require("./render");
+const reorder = require("./reorder");
 
 module.exports = function() {
   program.version("1.0.0");
@@ -14,6 +15,11 @@ module.exports = function() {
     .command("render")
     .description("Render a complete style.json from parts.")
     .action(render);
+
+  program
+    .command("matchorder")
+    .description("Match layer order between different style files")
+    .action(reorder);
 
   program.parse(process.argv);
 };
