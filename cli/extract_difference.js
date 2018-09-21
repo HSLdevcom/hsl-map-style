2117fdff579608ecc9cd7eb9e3f0bfc70b4418d1
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-09-21 13:02:14 +0300

    Add commands for reordering and extracting differences.

---
+++ cli/extract_difference.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const fs = require("fs-extra");
+const get = require("lodash/get");
+const differenceBy = require("lodash/differenceBy");
+const merge = require("lodash/merge");
+
+module.exports = async function(file, compareFile) {
+  const filePath = path.resolve(process.cwd(), file);
+  const orderFilePath = path.resolve(process.cwd(), compareFile);
+
+  const fileJson = await fs.readJson(filePath);
+  const compareFileJson = await fs.readJson(orderFilePath);
+
+  const layers = get(fileJson, "layers", []);
+
+  if (layers.length === 0) {
+    throw new Error("No layers in file!");
+  }
+
+  const compareLayers = get(compareFileJson, "layers", []);
+
+  if (compareLayers.length === 0) {
+    throw new Error("No layers in order file!");
+  }
+
+  const diffLayers = differenceBy(layers, compareLayers, "id");
+
+  const diffFilejson = {
+    layers: diffLayers,
+  };
+
+  diffLayers.forEach((layer) => {
+    const source = get(layer, "source");
+    const mapboxGroup = get(layer, "metadata.mapbox:group", "");
+
+    if (source) {
+      merge(diffFilejson, {
+        sources: { [source]: fileJson.sources[source] },
+      });
+    }
+
+    if (mapboxGroup) {
+      merge(diffFilejson, {
+        metadata: {
+          "mapbox:groups": {
+            [mapboxGroup]: fileJson.metadata["mapbox:groups"][mapboxGroup],
+          },
+        },
+      });
+    }
+  });
+
+  const newLayers = layers.filter((layer) => {
+    const isDiff = diffLayers.find((dl) => dl.id === layer.is);
+    return !isDiff;
+  });
+
+  fileJson.layers = newLayers;
+
+  await fs.writeJson(filePath, fileJson, { spaces: 2 });
+  await fs.writeJson(
+    path.resolve(path.dirname(filePath), `layerdiff_${path.basename(file)}`),
+    diffFilejson,
+    { spaces: 2 }
+  );
+  
+  process.exit(0);
+};
--- cli/index.js
+++ cli/index.js
@@ -2,6 +2,7 @@ const program = require("commander");
 const split = require("./split");
 const render = require("./render");
 const reorder = require("./reorder");
+const extract_difference = require("./extract_difference");
 
 module.exports = function() {
   program.version("1.0.0");
@@ -20,6 +21,13 @@ module.exports = function() {
     .command("matchorder")
     .description("Match layer order between different style files")
     .action(reorder);
+
+  program
+    .command("extract-diff")
+    .description(
+      "Separate layers from file A that don't exist in file B. Modifies file A."
+    )
+    .action(extract_difference);
 
   program.parse(process.argv);
 };
