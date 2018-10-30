78fdb9f5d8a88305251b2ae3fd54eb362851ee5c
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-10-30 13:07:08 +0200

    Add layers and a simple-style output file.

--- cli/render.js
+++ cli/render.js
@@ -4,13 +4,15 @@ module.exports = function() {
   const styleJson = style.generateStyle({
     glyphsUrl: "https://kartat.hsldev.com/",
     components: {
-      routes: { enabled: true },
-      stops: { enabled: true },
-      citybikes: { enabled: true },
-      municipal_borders: { enabled: true },
-      poi: { enabled: true },
-      ticket_sales: { enabled: true },
-      driver_instructions: { enabled: true },
+      routes: { enabled: false },
+      stops: { enabled: false },
+      citybikes: { enabled: false },
+      municipal_borders: { enabled: false },
+      poi: { enabled: false },
+      ticket_sales: { enabled: false },
+      driver_instructions: { enabled: false },
+      icons: { enabled: true },
+      text_fisv: { enabled: true },
     },
   });
 
